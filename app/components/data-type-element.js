04050c3d5696583c7b04bc2a98155c8a1d6b540c
jswob <jacswob@gmail.com>
2020-11-13 09:40:37 +0100

    Create data-type-element component

---
+++ app/components/data-type-element.js
@@ -0,0 +1,6 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+
+export default class DataTypeElementComponent extends Component {
+  @service searchAttributes;
+}
